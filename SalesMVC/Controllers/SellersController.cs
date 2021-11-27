1a357e2d34a3a69ae687a200a4acb194600b6a32
Fabiana Angelo <fabianna.info@gmail.com>
2021-11-27 16:38:06 -0300

    Tela simples de cadastro

--- SalesMVC/Controllers/SellersController.cs
+++ SalesMVC/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesMVC.Services;
+using SalesMVC.Models;
 
 namespace SalesMVC.Controllers
 {
@@ -20,6 +21,17 @@ namespace SalesMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
