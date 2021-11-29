1c76d7d3c5bc89ae931197a0793817157cfbfdad
Fabiana Angelo <fabianna.info@gmail.com>
2021-11-29 17:56:18 -0300

    Implementando busca simples

--- SalesMVC/Startup.cs
+++ SalesMVC/Startup.cs
@@ -47,6 +47,7 @@ namespace SalesMVC
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
 
         }
 
